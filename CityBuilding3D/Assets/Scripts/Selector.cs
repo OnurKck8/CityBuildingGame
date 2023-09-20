2e6ad12a415ec38b822b098885f14dec2e85b532
Onur Küçük <124556360+OnurKck8@users.noreply.github.com>
2023-09-20 22:48:11 +0300

    UI added and GameManager started
    Clicked Mouse Position

--- CityBuilding3D/Assets/Scripts/Selector.cs
+++ CityBuilding3D/Assets/Scripts/Selector.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Selector : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Camera cam;
+    public static Selector Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
     void Start()
     {
-        
+        cam = Camera.main;
     }
 
-    // Update is called once per frame
-    void Update()
+    //zemine erismek icin
+    public Vector3 GetCurrentTilePos()
     {
-        
+        if(EventSystem.current.IsPointerOverGameObject())//UI'a degdiyse-Canvasa dokundaysa
+        {
+            return new Vector3(0,-99,0);//Bir sey yapma
+        }
+
+        Plane plane = new Plane(Vector3.up, Vector3.zero);//Hayali bir plane olusturduk.
+
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        float rayOut = 0.0f;
+
+        if(plane.Raycast(ray,out rayOut))
+        {
+            Vector3 newPos = ray.GetPoint(rayOut)-new Vector3(0.05f,0.0f,0.5f);//Hafif offset yapmak icin
+            newPos = new Vector3(Mathf.CeilToInt(newPos.x),0.0f, Mathf.CeilToInt(newPos.z));//Uste tamamlamak icin. 2.99=>3.00 yap
+            return newPos;
+        }
+
+        return new Vector3(0,-99,0);
     }
 }
