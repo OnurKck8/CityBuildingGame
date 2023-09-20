69bf428f8643a88985133dee74b6ad4aaa8da30e
Onur Küçük <124556360+OnurKck8@users.noreply.github.com>
2023-09-20 10:20:15 +0300

    Camera Controller Added
    Building Presets Added

--- CityBuilding3D/Assets/Scripts/CameraController.cs
+++ CityBuilding3D/Assets/Scripts/CameraController.cs
@@ -4,15 +4,66 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Camera cam;
+
+    [Header("Move")]
+    public float moveSpeed;
+
+    [Header("Rotate")]
+    public float rotateSpeed;
+    public float minXRot;
+    public float maxXRot;
+    private float curXRot;
+
+    [Header("Zoom")]
+    public float zoomSpeed;
+    public float minZoom;
+    public float maxZoom;
+    private float curZoom;
+
+  
+    private void Start()
     {
-        
+        cam = Camera.main;
+
+        curZoom = cam.transform.localPosition.y;
+        curXRot = -50;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        curZoom += Input.GetAxis("Mouse ScrollWheel") * -zoomSpeed;
+        curZoom = Mathf.Clamp(curZoom, 20, 50);//curZoom = Mathf.Clamp(curZoom, minZoom, maxZoom);
+
+        cam.transform.localPosition = Vector3.up * curZoom;
+
+        if(Input.GetMouseButton(1))
+        {
+            float x = Input.GetAxis("Mouse X");
+            float y = Input.GetAxis("Mouse Y");
+
+            curXRot += -y * rotateSpeed;
+            curXRot = Mathf.Clamp(curXRot, minXRot, maxXRot);//curXRot = Mathf.Clamp(curXRot, minXRot, maxXRot);
+
+            transform.eulerAngles = new Vector3(curXRot, transform.eulerAngles.y + (x + rotateSpeed), 0.0f);
+        }
+
+        //Movement
+        Vector3 forward = cam.transform.forward;
+        forward.y = 0.0f;//Yukari dogru giderken Y de hareket etme
+        forward.Normalize();
+
+        Vector3 right = cam.transform.right;
+
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveZ = Input.GetAxisRaw("Vertical");
+
+        Vector3 dir = forward * moveZ + right * moveX;
+        dir.Normalize();
+
+        dir *= moveSpeed * Time.deltaTime;
+
+        transform.position += dir;
+
     }
 }
