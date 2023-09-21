9c6dd8f0dc868d841a9e4a09276fd20ffcb00604
Onur Küçük <124556360+OnurKck8@users.noreply.github.com>
2023-09-21 22:29:16 +0300

    Finished BuildingPlacement

--- CityBuilding3D/Assets/Scripts/BuildingPlacement.cs
+++ CityBuilding3D/Assets/Scripts/BuildingPlacement.cs
@@ -4,15 +4,88 @@ using UnityEngine;
 
 public class BuildingPlacement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private bool currentlyPlacing;//Orada bir sey var mi?
+    private bool currentlyBulldozering;//Su an silinmis mi
+
+    private BuildingPreset curBuilidngPreset;
+
+    private float indicatorUpdateTime = 0.05f;//Gecikme suresi veriyor ki. Ýslemci yorulmasin
+    private float lastUpdateTime;
+    private Vector3 curIndicatorPos;
+
+    public GameObject placementIndicator;
+    public GameObject bulldozerIndicator;
+
+    public void BeginNewBuildingPlacement(BuildingPreset preset)
     {
-        
+        //if(City.Instance.money<preset.cost)
+        //{
+        //    return;
+        //}
+
+        currentlyPlacing = true;
+        curBuilidngPreset = preset;
+        placementIndicator.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CancelBuildingPlacement()
     {
-        
+        currentlyPlacing = false;
+        placementIndicator.SetActive(false);
+    }
+
+    public void ToggleBulldoze()
+    {
+        currentlyBulldozering = !currentlyBulldozering;//Ne secildiyse diger duruma gec
+        bulldozerIndicator.SetActive(currentlyBulldozering);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            CancelBuildingPlacement();
+        }
+
+        if(Time.time-lastUpdateTime>indicatorUpdateTime)
+        {
+            lastUpdateTime = Time.time;
+
+            curIndicatorPos = Selector.Instance.GetCurrentTilePos();//Tam yerlestirme
+
+            if(currentlyPlacing)
+            {
+                placementIndicator.transform.position = curIndicatorPos;
+            }
+            else if(currentlyBulldozering)
+            {
+                bulldozerIndicator.transform.position = curIndicatorPos;
+            }
+        }
+
+        if(Input.GetMouseButtonDown(0) && currentlyPlacing)//musaitse orasi
+        {
+            PlaceBuilding();
+        }
+        else if(Input.GetMouseButtonDown(0) && currentlyBulldozering)
+        {
+            Bulldoze();
+        }
+    }
+
+    void PlaceBuilding()
+    {
+        GameObject buildingObj = Instantiate(curBuilidngPreset.prefab, curIndicatorPos, Quaternion.identity);
+        City.Instance.OnPlaceBuilding(buildingObj.GetComponent<Building>());
+        CancelBuildingPlacement();
+    }
+
+    void Bulldoze()
+    {
+        Building buildingToDestroy = City.Instance.buildings.Find(x => x.transform.position == curIndicatorPos);
+        if(buildingToDestroy!=null)
+        {
+            City.Instance.OnRemoveBuilding(buildingToDestroy);
+        }
     }
 }
--- CityBuilding3D/Assets/Scripts/City.cs
+++ CityBuilding3D/Assets/Scripts/City.cs
@@ -1,19 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 
 public class City : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    public static City Instance;
 
+    public int money;
+    public int day;
+    public int curPopulation;
+    public int curJobs;
+    public int curFood;
+    public int maxPopulation;
+    public int maxJobs;
+    public int incomePerJob;//Is basi gelir
+
+    public TextMeshProUGUI statsText;
+    public List<Building> buildings = new List<Building>();
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    public void OnPlaceBuilding(Building building)
+    {
+        money -= building.buildingPreset.cost;
+        maxPopulation += building.buildingPreset.population;
+        maxJobs += building.buildingPreset.jobs;
+        buildings.Add(building);
+        UpdateStatText();
+    }
+    public void OnRemoveBuilding(Building building)
+    {
+        maxPopulation -= building.buildingPreset.population;
+        maxJobs -= building.buildingPreset.jobs;
+        buildings.Remove(building);
+        Destroy(building.gameObject);
+        UpdateStatText();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
 
+    private void UpdateStatText()
+    {
+        
     }
 }
